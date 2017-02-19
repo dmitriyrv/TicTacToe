9c7ec3379246ac79d0cc0fa64573180878c12f76
Dmitriy Vasiliev <dmitriyrv@gmail.com>
2017-02-19 11:06:39 +0200

    Initial commit with app structure

---
+++ src/main/java/com/tictactoe/TicTacToeApplication.java
@@ -0,0 +1,12 @@
+package com.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeApplication.class, args);
+	}
+}
